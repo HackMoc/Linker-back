1f2e3afe624d27aeea9272fe2038c80f28c62f35
Márcio Eduardo Gomes Barbosa (MarcioRGB) <marcio@aptans.com>
2018-08-18 16:53:57 -0300

    Modifica views
    - adiciona a url de login
    - adiciona testes da view de login

--- linker/accounts/tests.py
+++ linker/accounts/tests.py
@@ -1,19 +1,30 @@
 from django.test import Client, TestCase
 from .models import User
+from django.urls import reverse
 
 class ModelsTestCase(TestCase):
 
     def test_model_instance(self):
         username = User()
-
         self.assertIsInstance(username, User)
 
     def teste_field_not_blank(self):
-        username = User.objects.create(
-            last_name='mcdonald',
+        user = User.objects.create(
+            email='jhon@gmail.com',
+        )
+        user.save()
+        self.assertNotEqual(user.username, None)
+
+    def test_verifica_password_of_user(self):
+        user = User.objects.create(
             username='jhon',
             email='jhon@gmail.com',
         )
         username.save()
+class ViewTestCase(TestCase):
 
         self.assertNotEqual(username.first_name, None)
+    def test_login_view_status_code_200(self):
+        c = Client()
+        response = c.post(reverse('accounts:login'), {'username': 'chris', 'password': 'test'})
+        self.assertEqual(response.status_code, 200)
--- linker/accounts/views.py
+++ linker/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+    def post(self, request):
+        return HttpResponse('result', status=200)
+
